2c97d459413a0357101fabcf6e2f1af8f4e1e176
KIM JUNGHWAN <writer0713@naver.com>
2019-07-14 16:23:07 +0900

    #step1 [test] PlayerTest 추가 (test fail)
    Player가 볼링을 play 하는 테스트 추가

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PlayerTest {
+
+  @Test
+  void play() {
+    final String name = "kjh";
+    Player player = new Player(name);
+
+    int score = player.play();
+    assertThat(score).isEqualTo(5);
+  }
+}
