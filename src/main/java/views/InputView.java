4cb75ad77e14b00ab3e73082d8369ffeb4264beb
KIM JUNGHWAN <writer0713@naver.com>
2019-07-14 16:05:47 +0900

    #step1 [feature] player 이름 알파벳 3글자가 아닐시 exception 발생

--- src/main/java/views/InputView.java
+++ src/main/java/views/InputView.java
@@ -1,17 +1,27 @@
 package views;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 import java.util.Scanner;
 
 public class InputView {
 
-  private static final String MESSAGE_FOR_PLAYER_NAME = "플레이어 이름은(3 english letters)? : ";
+  private static final String MESSAGE_FOR_ASKING_PLAYER_NAME = "플레이어 이름은(3 english letters)? : ";
+  private static final String ERROR_MESSAGE_FOR_ILLEGAL_PLAYER_NAME = "사용자의 이름은 알파벳 3글자로 구성되어야 합니다.";
   private static final Scanner scanner = new Scanner(System.in);
 
 
   public static String askPlayerName() {
-    System.out.printf("%s", MESSAGE_FOR_PLAYER_NAME);
+    System.out.printf("%s", MESSAGE_FOR_ASKING_PLAYER_NAME);
 
     String name = scanner.nextLine();
+    validPlayerName(name);
     return name;
+  }
+
+  private static void validPlayerName(String name) {
+    if (name.length() != 3) {
+      throw new IllegalArgumentException(ERROR_MESSAGE_FOR_ILLEGAL_PLAYER_NAME);
+    }
   }
 }
