ce47e397236b61862daa586e17fdf1fb2cdc1f46
KIM JUNGHWAN <writer0713@naver.com>
2019-07-14 16:02:35 +0900

    #step1 [refactor] InputView를 통해 player 이름 입력받도록 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
+import views.InputView;
 
 public class Application {
 
   public static void main(String[] args) {
-    System.out.println("main");
 
-    System.out.print("플레이어 이름은(3 english letters)?: ");
-
-    Scanner scanner = new Scanner(System.in);
-    String name = scanner.nextLine();
+    String name =InputView.askPlayerName();
 
     System.out.println("name : " + name);
   }
