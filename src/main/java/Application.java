15cca991021bdc0a5bb7353558c269c2d956b59c
KIM JUNGHWAN <writer0713@naver.com>
2019-07-14 15:58:46 +0900

    #step1 [feature] 플레이어 이름 입력 받기 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Application {
 
   public static void main(String[] args) {
     System.out.println("main");
+
+    System.out.print("플레이어 이름은(3 english letters)?: ");
+
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+
+    System.out.println("name : " + name);
   }
 }
